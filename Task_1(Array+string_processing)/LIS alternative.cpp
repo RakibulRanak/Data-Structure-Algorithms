36cd90779842de577fb030d0d888d5b9df7e8ff8
RakibulRanak <43205114+RakibulRanak@users.noreply.github.com>
2019-09-15 02:05:15 +0600

    Update LIS alternative.cpp

--- Task_1(Array+string_processing)/LIS alternative.cpp
+++ Task_1(Array+string_processing)/LIS alternative.cpp
@@ -4,42 +4,81 @@
 using namespace std;
 
 
-int longOddEvenIncSeq(int arr[], int n)
+int main()
 {
-
+	int ara[] = { 1, 2, 4, 4, 5, 5, 7, 8};
+	int n = sizeof(ara) / sizeof(n);
 	int lioes[n];
 
 	int maxLen = 0;
+	int lastindex;
+	bool odd = false, even = false;
 
 	for (int i = 0; i < n; i++)
 		lioes[i] = 1;
 
 
-	for (int i = 1; i < n; i++){
-		for(int i=0;i<n;i++)
-			cout<<lioes[i]<<" ";
-		cout<<endl;
-		for (int j = 0; j < i; j++)
-			if (arr[i] > arr[j] && (arr[i] + arr[j]) % 2 != 0 && lioes[i] < lioes[j] + 1)
+	for (int i = 1; i < n; i++) {
+		for (int i = 0; i < n; i++)
+			cout << lioes[i] << " ";
+
+		cout << endl;
+
+		for (int j = 0; j < i; j++) {
+			if (ara[i] > ara[j] && (ara[i] + ara[j]) % 2 != 0 && lioes[i] < lioes[j] + 1) {
 				lioes[i] = lioes[j] + 1;
+				if (lioes[i] > maxLen) {
+					maxLen = lioes[i];
+					lastindex = i;
+					if (lioes[i] % 2 == 1) {
+						odd = true;
+						even = false;
+
+					}
+					else
+					{
+						odd = false;
+						even = true;
+					}
+
+				}
+			}
+		}
 	}
+	for(int i=0;i<n;i++)
+		cout<<lioes[i]<<" ";
+	cout<<endl;
+	cout << maxLen << endl;
+	vector<int>vec;
+	int max = maxLen;
+	int value = ara[lastindex];
+	int i = lastindex;
+	while (max > 0)
+	{
+		if ((i == lastindex || value > ara[i]) && even &&ara[i]%2==0)
+		{
+			max--;
+			vec.push_back(ara[i]);
+			value = ara[i];
+			even = false;
+			odd = true;
+			//cout<<"hieven"<<endl;
 
+		}
+		else if ((i == lastindex || value > ara[i]) && odd &&ara[i]%2==1)
+		{
+			max--;
+			vec.push_back(ara[i]);
+			value = ara[i];
+			even = true;
+			odd = false;
+			//cout<<"hiodd"<<endl;
+		}
+		i--;
+	}
+	for (int i = vec.size() - 1; i >= 0; i--)
+		cout << vec[i] << " ";
+	cout << endl;
 
-	for (int i = 0; i < n; i++)
-		if (maxLen < lioes[i])
-			maxLen = lioes[i];
-
-
-	return maxLen;
-}
-
-
-int main()
-{
-	int arr[] = { 1, 2, 4, 4, 5, 5, 6, 8};
-	int n = sizeof(arr) / sizeof(n);
-	cout << "Longest Increasing Odd Even "
-	     << "Subsequence: "
-	     << longOddEvenIncSeq(arr, n);
 	return 0;
 }
